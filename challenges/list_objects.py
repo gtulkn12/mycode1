0fc1f4a0e42bd3dc54647fe9a6a3de17231c39a1
Malik Stuckey <gtulkn1@yahoo.com>
2021-05-10 21:36:11 +0000

    list_objects challenge

---
+++ challenges/list_objects.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+icecream= ["flavors", "salty"]
+icecream.append(99)
+name= input("Please ente your name. ")
+print(f"{icecream[2]} {icecream[0]}, and {name} chooses to be {icecream[1]}") 
