f3af5a9ded45c5c8a484eb6fca525f924769a60d
Malik Stuckey <gtulkn1@yahoo.com>
2021-05-10 19:50:43 +0000

    added Lab 8 User Input and Input Challenge

---
+++ challenges/user_input.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# print question
+day= input("What day of the week is it?\n>")
+name= input("What is your name?\n>")
+print(f"Hello, {name}! Happy {day}!")
+
+
+# PRINT OBJECTS
+print("Hello, ",name,"! Happy ",day,"!", sep="")
+# CONCATENATION
+print("Hello, " + name + "! Happy " + day + "!")
+# FORMAT METHOD
+print("Hello, {}! Happy {}!".format(name, day))
+# F-STRING
+print(f"Hello, {name}! Happy {day}!")
